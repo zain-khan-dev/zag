965639a8a182ccb8f7e65f39becd1d8e5b552fa4
Zain Khan <zainkhanjune@gmail.com>
2022-06-07 22:23:27 +0530

    Updated to add visitor class to code generation

--- com/zain/tools/generateAST.py
+++ com/zain/tools/generateAST.py
@@ -1,5 +1,31 @@
 import sys
 
+
+
+def generate_visitor_interface(paramList, basename):
+
+    def add_visitor_class_def():
+        return f"""{add_indent(1)}interface Visitor<R>""" + "{\n"
+
+
+    def add_visitor_params():
+        print( basename)
+        params = []
+        for param in paramList:
+            currentParam = param.split(':')[0].strip()
+            
+            params.append(add_indent(2) + " R visit" + currentParam + basename + "(" + currentParam + " " + basename.lower() + ");\n ")
+
+        return ''.join(params) + add_indent(1) + "}"
+        # return ""
+
+
+    
+
+    return add_visitor_class_def() + add_visitor_params()
+
+
+
 def add_base_class(basename):
     return (f"abstract class {basename} "+ "{")
 
@@ -26,6 +52,10 @@ def add_constructor_body(paramList):
 
 
 
+def add_visitor_override(classname, basename):
+    return f"""{add_indent(2)}@Override\n""" + add_indent(2) + "<R> R accept(Visitor<R> visitor) {\n" + add_indent(4) + f"return visitor.visit{classname}{basename}(this);\n" + add_indent(2) + "}\n"
+
+
 def define_ast(output_dir, basename, types):
 
     newlinetabtab = '\n\t\t\t'
@@ -34,8 +64,11 @@ def define_ast(output_dir, basename, types):
 
     f = open(output_dir, "w+")
 
-    
+
     f.writelines(add_base_class(basename))
+    f.writelines("\n")
+
+    f.writelines(generate_visitor_interface(types, basename))
     f.writelines("\n")
     for type in types:
         
@@ -43,13 +76,19 @@ def define_ast(output_dir, basename, types):
 
         f.writelines(add_class_definition(basename, className))
         f.writelines("\n")
+        f.writelines("\n")
+        f.writelines(add_visitor_override(className.strip(), basename))
         paramList = type.split(":")[1].split(',')
         f.writelines(add_class_props(paramList))
         f.writelines("\n")
         f.writelines(add_class_constructor(className, paramList))
         f.writelines("\n")
         f.writelines(add_constructor_body(paramList))
+        
         f.writelines("\n")
+
+    f.writelines("\n")
+    f.writelines(add_indent(1) + "abstract <R> R accept(Visitor<R> visitor);")
 
     f.writelines("\n}")
 
@@ -60,13 +99,13 @@ if __name__ == '__main__':
         exit()
 
     output_dir = sys.argv[1]
-    print(output_dir)
-    define_ast(output_dir,
-     "Expr", 
-     [
+    paramList = [
          "Binary   : Expr left, Token operator, Expr right",
          "Grouping  : Expr Expression",
          "Literal  : Object value",
          "Unary  : Token operator, Expr right" 
      ]
-     )
+    basename = "Expr"
+    print(output_dir)
+    define_ast(output_dir,basename, paramList)
+    # print(generate_visitor_interface(paramList, basename))
